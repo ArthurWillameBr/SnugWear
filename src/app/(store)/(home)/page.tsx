d02dfda9680aac55d1d1bbf407bc763bfa7a2665
arthurwillame5 <arthurwillame2005@gmail.com>
2024-06-07 19:50:59 -0300

    feat: Add lazy loading for product images on home page

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -1,63 +1,86 @@
+import { api } from "@/data/api";
+import { Product } from "@/data/types/products";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+async function getFeaturedProducts(): Promise<Product[]> {
+  const response = await api("/products/featured");
+
+  const products = await response.json();
+
+  return products;
+}
+
+export default async function Home() {
+  const products = await getFeaturedProducts();
+
+  const [highlightedProduct, ...otherProducts] = products;
+
   return (
     <main className="grid max-h-[860px] grid-cols-9 grid-rows-6 gap-6">
+       <Link
+       href={`/products/${highlightedProduct.slug}`}
+       className="group flex relative items-start justify-center col-span-6 row-span-6 rounded-lg bg-zinc-900 overflow-hidden"
+     >
+       <Image
+         className="group-hover:scale-105 transition-transform duration-500"
+         src={highlightedProduct.image}
+         alt=""
+         width={920}
+         height={920}
+         quality={100}
+       />
+       <div className="flex items-center gap-3 max-w-[280px] border-2 border-zinc-500 bg-black/60 p-1 pl-5 rounded-full absolute bottom-28 right-28 h-12  ">
+         <span className="text-sm truncate">{highlightedProduct.title}</span>
+         <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+           {highlightedProduct.price.toLocaleString("pt-BR", {
+              style: "currency",
+              currency: "BRL",
+           })}
+         </span>
+       </div>
+     </Link>
       <Link
-        href="/"
-        className="group flex relative items-start justify-center col-span-6 row-span-6 rounded-lg bg-zinc-900 overflow-hidden"
-      >
-        <Image
-          className="group-hover:scale-105 transition-transform duration-500"
-          src="/moletom-never-stop-learning.png"
-          alt=""
-          width={920}
-          height={920}
-          quality={100}
-        />
-        <div className="flex items-center gap-3 max-w-[280px] border-2 border-zinc-500 bg-black/60 p-1 pl-5 rounded-full absolute bottom-28 right-28 h-12  ">
-          <span className="text-sm truncate">Moleton never stop</span>
-          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
-            R$ 99,99
-          </span>
-        </div>
-      </Link>
-      <Link
-        href="/"
+        href={`products/${otherProducts[0].slug}`}
         className="group relative flex items-start justify-center col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden"
       >
         <Image
           className="group-hover:scale-105 transition-transform duration-500"
-          src="/moletom-ia-p-devs.png"
+          src={otherProducts[0].image}
           alt=""
           width={620}
           height={620}
           quality={100}
         />
         <div className="flex items-center gap-3 max-w-[280px] border-2 border-zinc-500 bg-black/60 p-1 pl-5 rounded-full absolute bottom-10 right-10 h-12  ">
-          <span className="text-sm truncate">Moleton never stop</span>
+          <span className="text-sm truncate">{otherProducts[0].title}</span>
           <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
-            R$ 99,99
+            {otherProducts[0].price.toLocaleString("pt-BR", {
+              style: "currency",
+              currency: "BRL",
+           })}
           </span>
         </div>
       </Link>
       <Link
-        href="/"
+        href={`/products/${otherProducts[1].slug}`}
         className="group relative flex items-start justify-center col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden"
       >
         <Image
           className="group-hover:scale-105 transition-transform duration-500"
-          src="/camiseta-dowhile-2022.png"
+          src={otherProducts[1].image}
           alt=""
           width={620}
           height={620}
           quality={100}
         />
         <div className="flex items-center gap-3 max-w-[280px] border-2 border-zinc-500 bg-black/60 p-1 pl-5 rounded-full absolute bottom-10 right-10 h-12  ">
-          <span className="text-sm truncate">Moleton never stop</span>
+          <span className="text-sm truncate">{otherProducts[1].title}</span>
           <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
-            R$ 99,99
+            {otherProducts[1].price.toLocaleString("pt-BR", {
+              style: "currency",
+              currency: "BRL",
+           })}
           </span>
         </div>
       </Link>
